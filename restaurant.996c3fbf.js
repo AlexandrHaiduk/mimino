24429248e6631b128b6846f2b289cc8efb453020
kuhai-anna <112816144+kuhai-anna@users.noreply.github.com>
2022-12-15 14:22:36 +0000

    Deploying to gh-pages from @ kuhai-anna/mimino@255bfe95148ae0bff2ee45f109d9e96e500b7077 🚀

---
+++ restaurant.996c3fbf.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){$(".slider-right").slick({arrows:!0,dots:!1,adaptiveHeight:!0,slidesToShow:1,slidesToScroll:1,speed:1e3,infinite:!0,touchThreshold:20,waitForAnimate:!0,variableWidth:!0,rows:1,appendArrows:$(".slider-right__arrows")}),$(".slider-left").slick({arrows:!0,dots:!1,adaptiveHeight:!0,slidesToShow:1,slidesToScroll:1,speed:1e3,infinite:!0,touchThreshold:20,waitForAnimate:!0,variableWidth:!0,rows:1,appendArrows:$(".slider-left__arrows")})}));
+//# sourceMappingURL=restaurant.996c3fbf.js.map
